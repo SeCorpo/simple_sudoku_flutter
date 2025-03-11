a9f842040a46e70e669303177a0500266fda99f9
SeCorp-b <sem.koldewijn@student.hu.nl>
2025-03-11 09:43:13 +0100

    Logger removed singleton pattern, only static methods

--- lib/core/utils/logger.dart
+++ lib/core/utils/logger.dart
@@ -1,41 +1,40 @@
 enum LogLevel { verbose, debug, info, warning, error }
 
 class Logger {
-  static final Logger instance = Logger._internal(); // Direct static access
-  factory Logger() => instance; // Ensures the singleton pattern
+  Logger._(); // Private constructor prevents instantiation
 
-  LogLevel _logLevel = LogLevel.verbose;
-
-  Logger._internal();
-
-  static const Map<LogLevel, String> _logColors = {
-    LogLevel.verbose: '\x1B[37m', // White
-    LogLevel.debug: '\x1B[36m', // Cyan
-    LogLevel.info: '\x1B[32m', // Green
-    LogLevel.warning: '\x1B[33m', // Yellow
-    LogLevel.error: '\x1B[31m', // Red
-  };
+  static LogLevel _logLevel = LogLevel.verbose;
 
   static const String _resetColor = '\x1B[0m';
 
-  void setLogLevel(LogLevel level) {
+  static void setLogLevel(LogLevel level) {
     _logLevel = level;
   }
 
-  void log(LogLevel level, String message, {String? tag}) {
-    if (level.index < _logLevel.index) return;
-
-    final color = _logColors[level] ?? _resetColor;
-    final tagPrefix = tag != null ? '[$tag] ' : '';
-    final logMessage =
-        '$color${level.name.toUpperCase()}$_resetColor: $tagPrefix$message';
-
-    print(logMessage);
+  static void v(String message, {String? tag}) {
+    _log(LogLevel.verbose, '\x1B[37m', "VERBOSE", message, tag); // White
   }
 
-  static void v(String message, {String? tag}) => instance.log(LogLevel.verbose, message, tag: tag);
-  static void d(String message, {String? tag}) => instance.log(LogLevel.debug, message, tag: tag);
-  static void i(String message, {String? tag}) => instance.log(LogLevel.info, message, tag: tag);
-  static void w(String message, {String? tag}) => instance.log(LogLevel.warning, message, tag: tag);
-  static void e(String message, {String? tag}) => instance.log(LogLevel.error, message, tag: tag);
+  static void d(String message, {String? tag}) {
+    _log(LogLevel.debug, '\x1B[36m', "DEBUG", message, tag); // Cyan
+  }
+
+  static void i(String message, {String? tag}) {
+    _log(LogLevel.info, '\x1B[32m', "INFO", message, tag); // Green
+  }
+
+  static void w(String message, {String? tag}) {
+    _log(LogLevel.warning, '\x1B[33m', "WARNING", message, tag); // Yellow
+  }
+
+  static void e(String message, {String? tag}) {
+    _log(LogLevel.error, '\x1B[31m', "ERROR", message, tag); // Red
+  }
+
+  static void _log(LogLevel level, String color, String levelName, String message, String? tag) {
+    if (level.index < _logLevel.index) return; // Skip logs below the set level
+
+    final tagPrefix = tag != null ? '[$tag] ' : '';
+    print('$color$levelName$_resetColor: $tagPrefix$message');
+  }
 }
